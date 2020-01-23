b4dfe766a0ddc5cd5267dcadb85159e741875977
Chih-Hsiang Yeh <a9510130375@gmail.com>
2020-01-23 15:01:41 +0800

    New

--- Pictures_used_for_FCC_and_jets/Truth_particle_ID_PT_Correlation/ID_PT_correlation_projection.py
+++ Pictures_used_for_FCC_and_jets/Truth_particle_ID_PT_Correlation/ID_PT_correlation_projection.py
@@ -12,6 +12,7 @@ from ROOT import TMath
 from ROOT import TPaveText
 from ROOT import TLatex
 from array import array
+import numpy as np
 
 for Sort_particle in range(1,8):
     f1= ROOT.TFile.Open("/Users/ms08962476/singularity/TIming_Studies/tev5mm_pythia6_zprime5tev_qq_with_Eta_cut_for_component_check_truth.root",'r')
@@ -35,58 +36,73 @@ for Sort_particle in range(1,8):
     Fraction_WW=array("f",[])
     X_PT=array("f",[])
     X_Error=array("f",[])
-    Y_Error=array("f",[])
+    Y_Error_QQ=array("f",[])
+    Y_Error_WW=array("f",[])
 
     overfloat_bin_content_WW=0
     overfloat_bin_content_QQ=0
-    for Bin in range(1,10):
+    for Bin in range(1,7):
         
-        X_PT.append(-2.25+0.5*Bin)
-        X_Error.append(0)
-        Y_Error.append(0)
+        X_PT.append(-1.25+0.5*Bin)
+        X_Error.append(0.25)
 
-        if(Bin<9):
-            H1_Projection = h1.ProjectionX("1",Bin,Bin,"")
-            H2_Projection = h2.ProjectionX("2",Bin,Bin,"")
+        if(Bin>1 and Bin<6):
+            H1_Projection = h1.ProjectionX("1",Bin+2,Bin+2,"")
+            H2_Projection = h2.ProjectionX("2",Bin+2,Bin+2,"")
             if(H1_Projection.Integral()!=0):
                 H1_Projection.Sumw2()
                 H1_Projection.Scale(1/H1_Projection.Integral())
-                Fraction_QQ.append(H1_Projection.GetBinContent(Sort_particle))
-                print 'H1_Projection.GetBinContent(5): '+str(H1_Projection.GetBinContent(Sort_particle))
+                Fraction_QQ_Use=H1_Projection.GetBinContent(Sort_particle)
+                Fraction_QQ.append(Fraction_QQ_Use)
+                Y_Error_QQ.append(np.sqrt(H1_Projection.GetEntries()*Fraction_QQ_Use*(1-Fraction_QQ_Use))/H1_Projection.GetEntries())
             else:
                 Fraction_QQ.append(0)
-                print 'H1_Projection.GetBinContent(5): '+str(0)
+                Y_Error_QQ.append(0)
 
 
             if(H2_Projection.Integral()!=0):
                 H2_Projection.Sumw2()
                 H2_Projection.Scale(1/H2_Projection.Integral())
-                Fraction_WW.append(H2_Projection.GetBinContent(Sort_particle))
+                Fraction_WW_Use=H2_Projection.GetBinContent(Sort_particle)
+                Fraction_WW.append(Fraction_WW_Use)
+                Y_Error_WW.append(np.sqrt(H2_Projection.GetEntries()*Fraction_WW_Use*(1-Fraction_WW_Use))/H2_Projection.GetEntries())
             else:
+                Y_Error_WW.append(0)
                 Fraction_WW.append(0)
                    
-        if(Bin==9):
-            H1_Projection = h1.ProjectionX("1",Bin,Bin+1,"")
-            H2_Projection = h2.ProjectionX("2",Bin,Bin+1,"")
+        if(Bin==1 or Bin==6):
+            if(Bin==1):
+                H1_Projection = h1.ProjectionX("1",Bin,Bin+1,"")
+                H2_Projection = h2.ProjectionX("2",Bin,Bin+1,"")
+            if(Bin==6):
+                H1_Projection = h1.ProjectionX("1",Bin+2,Bin+4,"")
+                H2_Projection = h2.ProjectionX("2",Bin+2,Bin+4,"")
+
             if(H1_Projection.Integral()!=0):
                 H1_Projection.Sumw2()
                 H1_Projection.Scale(1/H1_Projection.Integral())
-                Fraction_QQ.append(H1_Projection.GetBinContent(Sort_particle))
+                Fraction_QQ_Use=H1_Projection.GetBinContent(Sort_particle)
+                Fraction_QQ.append(Fraction_QQ_Use)
+                Y_Error_QQ.append(np.sqrt(H1_Projection.GetEntries()*Fraction_QQ_Use*(1-Fraction_QQ_Use))/H1_Projection.GetEntries())
             #print 'H1_Projection.GetBinContent(5): '+str(H1_Projection.GetBinContent(Sort_particle))
             else:
                 Fraction_QQ.append(0)
+                Y_Error_QQ.append(0)
             #   print 'H1_Projection.GetBinContent(5): '+str(0)
     
     
             if(H2_Projection.Integral()!=0):
                 H2_Projection.Sumw2()
                 H2_Projection.Scale(1/H2_Projection.Integral())
-                Fraction_WW.append(H2_Projection.GetBinContent(Sort_particle))
+                Fraction_WW_Use=H2_Projection.GetBinContent(Sort_particle)
+                Fraction_WW.append(Fraction_WW_Use)
+                Y_Error_WW.append(np.sqrt(H2_Projection.GetEntries()*Fraction_WW_Use*(1-Fraction_WW_Use))/H2_Projection.GetEntries())
             else:
-                Fraction_WW.append(0)
+                Fraction_QQ.append(0)
+                Y_Error_QQ.append(0)
 
 
-    gr_QQ = TGraphErrors(9,X_PT,Fraction_QQ,X_Error,Y_Error)
+    gr_QQ = TGraphErrors(6,X_PT,Fraction_QQ,X_Error,Y_Error_QQ)
     gr_QQ.SetLineColor(1)
     gr_QQ.SetLineWidth(1)
     gr_QQ.SetLineStyle(1)
@@ -96,7 +112,7 @@ for Sort_particle in range(1,8):
     gr_QQ.GetXaxis().SetTitle("Log(PT)")
     gr_QQ.GetYaxis().SetTitle("Arbitrary")
 
-    gr_WW = TGraphErrors(9,X_PT,Fraction_WW,X_Error,Y_Error)
+    gr_WW = TGraphErrors(6,X_PT,Fraction_WW,X_Error,Y_Error_WW)
     gr_WW.SetLineColor(3)
     gr_WW.SetLineWidth(1)
     gr_WW.SetLineStyle(1)
@@ -113,7 +129,7 @@ for Sort_particle in range(1,8):
 
     gr_QQ.GetHistogram().SetMaximum(1.2)
     gr_QQ.GetHistogram().SetMinimum(0)
-    gr_QQ.GetXaxis().SetLimits(-2,2.5)
+    gr_QQ.GetXaxis().SetLimits(-1,2)
     gr_QQ.GetXaxis().CenterTitle()
     gr_QQ.GetYaxis().CenterTitle()
 
